5d904be6cd8e246d55ebbaace44f921951df6724
Kavyasree-J-Ganesh <kavyasreejganesh97@gmail.com>
2022-11-23 14:42:16 +0530

    [Kavyasree J Ganes] Coupon problem

---
+++ src/logicalprograms/CouponNumbers.java
@@ -0,0 +1,20 @@
+package logicalprograms;
+
+public class CouponNumbers {
+    public static void main(String[] args) {
+        char[] chars="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789".toCharArray();
+        int max=100000000;
+        // String s = "";
+        int random=(int) (Math.random()*max);
+        StringBuffer sb = new StringBuffer();
+        while (random > 0){
+         //   int n = random % 10;
+         //   s+=String.valueOf(Chars[n]);
+            random = random/10;
+            sb.append(chars[ random % chars.length]);
+            random/= chars.length;
+        }
+        String str = sb.toString();
+        System.out.println(str);
+    }
+}
